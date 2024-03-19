e393e509fe63cc2611bd5fce68fd6e92ba3919ef
DaniloTom <148171407+DaniloTom@users.noreply.github.com>
2024-03-19 12:37:10 -0700

    Basic boid rules implemented, no bugs, but not working

--- boidSim.py
+++ boidSim.py
@@ -1,15 +1,27 @@
 import numpy as np
 import dcBoid
-
+import vpython as vp
 
 # Create a flock of boids
-flock = dcBoid.BoidFlock(3, True, True, True)
+flock = dcBoid.BoidFlock(3, 0.2, 0.2, 0.2)
 
-print(flock.boids[0].position)
-print(flock.allPositions[0, 0, :])
-print(flock.boids[1].position)
-print(flock.allPositions[0, 1, :])
-print(flock.boids[2].position)
-print(flock.allPositions[0, 2, :])
+# Simluation:
 
+numSteps = 100000
 
+boidObs = []
+
+for i in range(numSteps):
+
+    print(flock.allPositions[i, :, :])
+    flock.stepBoid()
+
+    for j in range(flock.numBoids):
+        if len(boidObs) < flock.numBoids:
+            boidObs.append(vp.sphere(pos = vp.vector(flock.allPositions[i, j, 0], flock.allPositions[i, j, 1], flock.allPositions[i, j, 2]), radius = flock.boids[j].size, color = vp.color.red))
+        else: 
+            boidObs[j].pos = vp.vector(flock.allPositions[i, j, 0], flock.allPositions[i, j, 1], flock.allPositions[i, j, 2])
+
+    vp.rate(60)
+
+print(flock.allPositions[0, :, :])
--- dcBoid.py
+++ dcBoid.py
@@ -3,10 +3,12 @@ import numpy as np
 
 class BoidFlock:
 
-    def __init__(self, num_boids, cohesion: 'np.ndarray' = False, alignment = False, separation = False):
+    def __init__(self, num_boids, cohesion = 0, alignment = 0, separation = 0, speed = 5, dt = 1/60):
         self.boids = []
+        self.step = 0
+        self.dt = dt
 
-        simStepsPerMemAlloc = 1000
+        self.simStepsPerMemAlloc = 1000
         self.numDim = 3
 
         self.cubeC1 = np.array([0,0,0])
@@ -18,10 +20,10 @@ class BoidFlock:
         self.separation = separation
 
         # Initialize the array to store all the positions of the boids
-        self.allPositions = np.full((simStepsPerMemAlloc, num_boids, self.numDim), np.nan)
+        self.allPositions = np.full((self.simStepsPerMemAlloc, num_boids, self.numDim), np.nan)
 
         # Initialize the array to store all the velocities of the boids
-        self.allVelocities = np.full((simStepsPerMemAlloc, num_boids, self.numDim), np.nan)
+        self.allUnitVeloc = np.full((self.simStepsPerMemAlloc, num_boids, self.numDim), np.nan)
 
         # Rows are time steps, columns are boids, and depth is x, y, z
 
@@ -31,19 +33,152 @@ class BoidFlock:
             self.allPositions[0, i, :] = boidPosition
 
             boidVelocity = np.random.uniform(0, 1, self.numDim)
-            self.allVelocities[0, i, :] = boidVelocity
+            self.allUnitVeloc[0, i, :] = boidVelocity/np.linalg.norm(boidVelocity)
 
-            self.boids.append(Boid(boidPosition, boidVelocity, i))
+            self.boids.append(Boid(boidPosition, boidVelocity, i, self.cohesion, self.alignment, self.separation, speed = speed))
 
-        self.boidsInit = True
         self.numBoids = num_boids
+
+    def stepBoid(self):
+
+        # Check if the arrays need to be expanded
+        if self.step == self.allPositions.shape[0] - 1:
+            self.allPositions = np.append(self.allPositions, np.full((self.simStepsPerMemAlloc, self.numBoids, self.numDim), np.nan), axis = 0)
+            self.allUnitVeloc = np.append(self.allUnitVeloc, np.full((self.simStepsPerMemAlloc, self.numBoids, self.numDim), np.nan), axis = 0)        
+        
+        # Move all the boids
+        for i in range(self.numBoids):
+            self.allPositions[self.step + 1, i, :], self.allUnitVeloc[self.step + 1, i, :] = self.boids[i].movement(self)
+
+        self.step += 1
+
 
         
 
 
 class Boid:
 
-    def __init__(self, position: 'np.ndarray', velocity: 'np.ndarray', boidID):
+    def __init__(self, position: 'np.ndarray', velocity: 'np.ndarray', boidID, alin, cohe, sep, inertia = 0.5, speed = 1):
         self.position = position
         self.velocity = velocity
         self.boidID = boidID
+        self.alignment = alin
+        self.cohesion = cohe
+        self.separation = sep
+        self.sightRadius = 10
+        self.speed = speed
+        self.size = 1
+        self.collisionTime = 0.3
+        self.inertia = inertia
+
+    def boidsInRadiusF(self, boidFlock: 'BoidFlock'):
+        boidsInRadius = []
+        for i in range(boidFlock.numBoids):
+            if i != self.boidID:
+                distance = np.linalg.norm(boidFlock.allPositions[boidFlock.step, i, :] - self.position)
+                if distance < self.sightRadius:
+                    boidsInRadius.append(i)
+        return boidsInRadius
+        
+    def cohesionF(self, boidFlock: 'BoidFlock', boidsInRadius):
+        # Return unit vector pointing from boid to average position of boids in the sight radius
+        # Check if there are any boids in the radius
+        if len(boidsInRadius) > 0:
+            averagePosition = np.mean(boidFlock.allPositions[boidFlock.step, boidsInRadius, :], axis = 0)
+            return (averagePosition - self.position) / np.linalg.norm(averagePosition - self.position)
+        else:
+            return np.array([0,0,0])
+
+    def alignmentF(self, boidFlock: 'BoidFlock', boidsInRadius):
+        # Return average heading of boids in the sight radius
+        if len(boidsInRadius) == 0:
+            return np.array([0,0,0])
+        else:
+            averageVelocity = np.mean(boidFlock.allUnitVeloc[boidFlock.step, boidsInRadius, :], axis = 0)
+
+        return averageVelocity / np.linalg.norm(averageVelocity)
+    
+    def separationF(self, boidFlock: 'BoidFlock', boidsInRadius):
+        
+        futurePosSelf = self.position + self.collisionTime * self.velocity
+
+        avoidanceList = np.full((len(boidsInRadius), 3), np.nan)
+        npI = 0
+        for i in boidsInRadius:
+            
+            # For each other boid, add the unit vector times the collision time to the position
+            futurePosOther = boidFlock.allPositions[boidFlock.step, i, :] + self.collisionTime * boidFlock.allUnitVeloc[boidFlock.step, i, :]
+            if np.linalg.norm(futurePosOther - futurePosSelf) < self.size:
+                dist2Impact = self.speed * self.collisionTime
+                vec2OtherImpact = futurePosOther - self.position
+                vec2SelfImpact = futurePosSelf - self.position
+                planeNormal = np.cross(vec2SelfImpact, vec2OtherImpact)
+                avoidanceVector = np.cross(vec2SelfImpact, planeNormal) * 0.2 + vec2SelfImpact * 0.8
+                avoidanceList[npI,:] = avoidanceVector / np.linalg.norm(avoidanceVector)
+                npI += 1
+            else:
+                # Remove a row from the avoidance list
+                avoidanceList = np.delete(avoidanceList[-1,:])
+        if npI == 0:
+            return np.array([0,0,0])
+        else:
+            return np.mean(avoidanceList, axis = 0) / np.linalg.norm(np.mean(avoidanceList, axis = 0))
+
+    def movement(self, boidFlock: 'BoidFlock'):
+        # Return the next position of the boid and unit vector of the next velocity
+
+        # Find birds in the radius 
+        boidsInRadius = self.boidsInRadiusF(boidFlock)
+
+        # Run cohesion
+        if self.cohesion != 0:
+            cohesionVector = self.cohesionF(boidFlock, boidsInRadius)
+        else:
+            cohesionVector = np.array([0,0,0])
+
+        if self.alignment != 0:
+            alignmentVector = self.alignmentF(boidFlock, boidsInRadius)
+        else:
+            alignmentVector = np.array([0,0,0])
+
+        if self.separation != 0:
+            separationVector = self.separationF(boidFlock, boidsInRadius)
+        else:
+            separationVector = np.array([0,0,0])
+
+        # Check if any of the entries in the vectors are NaN
+            
+        checkThese = np.concatenate((cohesionVector, alignmentVector, separationVector))
+        for i in range(len(cohesionVector) * 3):
+            if np.isnan(checkThese[i]):
+                # Raise an error
+                raise ValueError('One of the vectors is NaN')
+
+
+        newVelocity = self.cohesion * cohesionVector + self.alignment * alignmentVector + self.separation * separationVector + self.inertia * self.velocity
+
+        # Normalize the new velocity
+        newVelocity = newVelocity / np.linalg.norm(newVelocity)
+
+        newPosition = self.position + self.speed * newVelocity * boidFlock.dt
+
+        if newPosition[0] < boidFlock.cubeC1[0]:
+            newPosition[0] = boidFlock.cubeC2[0]
+        elif newPosition[0] > boidFlock.cubeC2[0]:
+            newPosition[0] = boidFlock.cubeC1[0]
+        
+        if newPosition[1] < boidFlock.cubeC1[1]:
+            newPosition[1] = boidFlock.cubeC2[1]
+        elif newPosition[1] > boidFlock.cubeC2[1]:
+            newPosition[1] = boidFlock.cubeC1[1]
+        
+        if newPosition[2] < boidFlock.cubeC1[2]:
+            newPosition[2] = boidFlock.cubeC2[2]
+        elif newPosition[2] > boidFlock.cubeC2[2]:
+            newPosition[2] = boidFlock.cubeC1[2]
+
+        return newPosition, newVelocity
+    
+    
+
+
